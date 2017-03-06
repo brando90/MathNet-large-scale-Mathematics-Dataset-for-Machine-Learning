82f2d54ac76e0af43411597b44e1a21edb757ca4
Brando <brando90@mit.edu>
2017-03-06 18:15:09 -0500

    first draft

--- main_proj_lib/main_eit_pkg/sample_problem_generation.py
+++ main_proj_lib/main_eit_pkg/sample_problem_generation.py
@@ -19,8 +19,15 @@ class Make_Var_Subject_Pilot(object):
 
         self.dropbox_proj_loc = '/Users/brandomiranda/Dropbox (MIT)/eit_proj1_data/algebra1/make_var_subj_pilot'
 
-    def get_making_the_subject_exercises(self,nb_ex):
-        pass
+    def get_making_the_subject_exercises(self,nb_ex=100):
+        for exercise_index in range(nb_ex):
+            problem_loc = self.dropbox_proj_loc+'/make_subject_pilot_problem_'+str(exercise_index)
+            soln_loc = self.dropbox_proj_loc+'/make_subject_pilot_soln_'+str(exercise_index)
+            with open(problem_loc,mode='w') as problem_file, open(soln_loc,mode='w') as soln_file:
+                nb_variables = np.random.randint(low=3,high=10)
+                problem_msg, soln_msg = self.get_single_new_exercise_sums(nb_variables,largest_int=100)
+                problem_file.write(problem_msg)
+                soln_file.write(soln_msg)
 
     def get_single_new_exercise_sums(self,nb_variables=26,largest_int=10):
         '''
@@ -51,18 +58,20 @@ class Make_Var_Subject_Pilot(object):
         else:
             # get solution
             variables_in_eq.remove(target_var)
+            print(variables_in_eq)
             lhs_variables = ''
             for i in range(0,len(variables_in_eq)-1):
                 var = variables_in_eq[i]
                 lhs_variables = lhs_variables + var + '+'
-            lhs_variables = lhs_variables + var
-            soln_expression = '%s - %s'%(str(integer),lhs_variables)
+            lhs_variables = lhs_variables + variables_in_eq[-1]
+            soln_expression = '%s = %s - %s'%(target_var[0],str(integer),lhs_variables)
             return soln_expression
 
 
 
 if __name__ == '__main__':
     maker_var_subject = Make_Var_Subject_Pilot()
-    problem_msg,soln_msg = maker_var_subject.get_single_new_exercise_sums(nb_variables=5)
-    print( problem_msg )
-    print( soln_msg )
+    # problem_msg,soln_msg = maker_var_subject.get_single_new_exercise_sums(nb_variables=5)
+    # print( problem_msg )
+    # print( soln_msg )
+    maker_var_subject.get_making_the_subject_exercises()
