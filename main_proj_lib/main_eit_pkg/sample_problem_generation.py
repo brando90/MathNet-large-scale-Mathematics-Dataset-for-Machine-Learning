078a5443a6a28f4e54dd10b08055dc982d676f46
Brando <brando90@mit.edu>
2017-03-06 17:38:42 -0500

    variable equation

--- main_proj_lib/main_eit_pkg/sample_problem_generation.py
+++ main_proj_lib/main_eit_pkg/sample_problem_generation.py
@@ -1,4 +1,5 @@
 import string
+import numpy as np
 
 import maps
 
@@ -19,9 +20,27 @@ class Make_Var_Subject(object):
     def get_making_the_subject_exercises(self,nb_ex):
         pass
 
-    def get_single_new_exercise_sums(self,random_seed):
+    def get_single_new_exercise_sums(self,nb_variables=26,largest_int=10):
         '''
         get new exercise with x_1 + x_2 + ... + x_N =  K and make some x_i the subject.
         '''
-        make_var_subj = self.var_names[i]
-        problem_msg = 'Problem: make %s the subject'
+        # variables in equation
+        variables_in_eq = np.random.choice( self.var_names, size=nb_variables, replace=False )
+        # choose target variable
+        target_var = np.random.choice( variables_in_eq, size=1 )
+        # create equation
+        equation = ''
+        for i in range(0,len(variables_in_eq)-1):
+            var = variables_in_eq[i]
+            equation = equation + var + '+'
+        equation = equation+variables_in_eq[-1]
+        equation = '%s=%d'%(equation,np.random.randint(0,high=largest_int))
+        # create problem msg
+        problem_msg = 'Problem: make %s the subject of equation: %s'%(target_var[0],equation)
+        return problem_msg
+
+
+if __name__ == '__main__':
+    maker_var_subject = Make_Var_Subject()
+    problem_msg = maker_var_subject.get_single_new_exercise_sums(nb_variables=5)
+    print( problem_msg )
