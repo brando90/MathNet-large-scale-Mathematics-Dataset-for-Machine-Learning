2e137bb13c955ae999c25c80007273f84d2e406a
Brando <brando90@mit.edu>
2017-03-13 18:52:37 -0400

    ckpt

--- main_proj_lib/main_eit_pkg/framework.py
+++ main_proj_lib/main_eit_pkg/framework.py
@@ -8,13 +8,17 @@ import pdb
 
 class Problem(object):
 
-    def __init__(self,names_to_function):
-        self.g = self.get_global_name(names_to_function)
-        pass
+    def __init__(self,):
+        #self.g = self.get_global_name(names_to_function)
+        #
+
 
     def get_global_name(self,names_to_function):
         # TODO: returns map obj mapping (name) to its corresponding (function)
         return
+
+    def generate_problem_statement(self):
+
 
 
 class Gen(object):
@@ -38,8 +42,8 @@ class Gen(object):
         return statement
 
 class SeqGen(Gen):
-    def __init__(self):
-        self.statements = [] # holds the functions that have the problem statements
+    def __init__(self,statements):
+        self.statements = statements # holds the functions that have the problem statements
 
     def get_statement(self,g):
         each_statements = self.process_chain(g)
@@ -47,33 +51,48 @@ class SeqGen(Gen):
         return statement
 
 class PerGen(Gen):
-    def __init__(self):
-        self.statements = [] # holds the functions that have the problem statements
+    def __init__(self,statements):
+        self.statements = statements # holds the functions that have the problem statements
 
     def get_statement(self,g):
         each_statements = self.process_chain(g)
+        each_statements = shuffle(each_statements) # TODO
         statement = self.join_statements(each_statements)
         return statement
 
 #
 
 def intro_sent(g):
-    setence = "%s is a rare disease in which the victim has the delusion that he or she is being subjected to intense examination."%(g.disease_name)
-    return setence
-
-def prob_disease_sent(array, random=None):
-
+    sentence = '%s is a rare disease in which the victim has the delusion that he or she is being subjected to intense examination.'%(g.disease_name)
     return sentence
 
-def prob_shaky_given_disease_sent():
-    pass
+def prob_disease_sent(g):
+    sentence = 'A person selected uniformly at random has %s with probability %f'%(g.disease_name,g.p_f)
+    return sentence
 
-def prob_shaky_given_no_disease_sent():
-    pass
+def prob_shaky_given_disease_sent(g):
+    sentence = 'A person with %s has %ss with probability %f'(g.disease_name,g.symptom,g.p_s_f)
+    return sentence
+
+def prob_shaky_given_no_disease_sent(g):
+    sentence = 'A person without %s has %s with probability %f'(g.disease_name,g.symptom,g.p_s_f)
+    return sentence
+
+#
+
+def get_disease_names():
+    diseases = ['CSphobia','Biophobia','Mathphobia'] #TODO: re-think
+    #
+    
+    return
 
 ##
 
+def probability_example_problem():
+    first_seq_gen = [intro_sent]
+    permutation_gen = [prob_disease_sent,prob_shaky_given_disease_sent,prob_shaky_given_no_disease_sent]
+    #
+    key_words_values = {'disease_name':}
+    return
+
 if __name__ = '__main__':
-    d = maps.NamedDict()
-    disease_name = ['Quizphobia','Biophobia','Mathphobia','CSphobia']
-    d.f = intro_sentence
