4edc7791c350dddd58681f97a487988c00a6ef46
Brando <brando90@mit.edu>
2017-03-12 22:37:43 -0400

    treat

---
+++ main_proj_lib/main_eit_pkg/framework.py
@@ -0,0 +1,32 @@
+import string
+import numpy as np
+
+import maps
+
+import pdb
+
+
+
+
+#
+
+def intro_sent(array,random=None):
+    setence = "%s is a rare disease in which the victim has the delusion that he or she is being subjected to intense examination."
+    return setence
+
+def prob_disease_sent(array, random=None):
+
+    return sentence
+
+def prob_shaky_given_disease_sent():
+    pass
+
+def prob_shaky_given_no_disease_sent():
+    pass
+
+##
+
+if __name__ = '__main__':
+    d = maps.NamedDict()
+    disease_name = ['Quizphobia','Biophobia','Mathphobia','CSphobia']
+    d.f = intro_sentence
