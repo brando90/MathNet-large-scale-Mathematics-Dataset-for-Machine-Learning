6eca98aa13fe004c12a2e2ca2cc098f764f5d9ea
Brando <brando90@mit.edu>
2017-03-13 20:38:46 -0400

    soln

--- main_proj_lib/main_eit_pkg/framework.py
+++ main_proj_lib/main_eit_pkg/framework.py
@@ -1,4 +1,6 @@
 import string
+import random
+
 import numpy as np
 
 import maps
@@ -8,18 +10,36 @@ import pdb
 
 class Problem(object):
 
-    def __init__(self,):
-        #self.g = self.get_global_name(names_to_function)
-        #
-
-
-    def get_global_name(self,names_to_function):
-        # TODO: returns map obj mapping (name) to its corresponding (function)
-        return
+    def __init__(self,array_generators,key_words_values,solution_creater):
+        self.array_generators = array_generators
+        self.key_words_values = key_words_values
+        self.g = maps.NamedDict()
+        self.solution_creater = solution_creater
 
     def generate_problem_statement(self):
+        '''
+        Generates a specific problem
+        '''
+        self.sample_random_state()
+        #
+        question = self.generate_statements()
+        return
 
+    def sample_random_state(self):
+        d = {}
+        for k,v in self.key_words_values():
+            d[k] = v()
+        self.g = maps.NamedDict(d)
 
+    def generate_statements(self):
+        problem_statement = ''
+        for gen in self.array_generators():
+            problem_statement = problem_statement + gen.get_statement(self.g)
+        return problem_statement
+
+    def generate_solution(self,g):
+        soln = self.solution_creater(g)
+        return soln
 
 class Gen(object):
 
@@ -42,6 +62,7 @@ class Gen(object):
         return statement
 
 class SeqGen(Gen):
+
     def __init__(self,statements):
         self.statements = statements # holds the functions that have the problem statements
 
@@ -51,12 +72,13 @@ class SeqGen(Gen):
         return statement
 
 class PerGen(Gen):
+
     def __init__(self,statements):
         self.statements = statements # holds the functions that have the problem statements
 
     def get_statement(self,g):
         each_statements = self.process_chain(g)
-        each_statements = shuffle(each_statements) # TODO
+        each_statements = random.shuffle(each_statements)
         statement = self.join_statements(each_statements)
         return statement
 
@@ -71,28 +93,60 @@ def prob_disease_sent(g):
     return sentence
 
 def prob_shaky_given_disease_sent(g):
-    sentence = 'A person with %s has %ss with probability %f'(g.disease_name,g.symptom,g.p_s_f)
+    sentence = 'A person with %s has %s with probability %f'(g.disease_name,g.symptom,g.p_s_f)
     return sentence
 
 def prob_shaky_given_no_disease_sent(g):
-    sentence = 'A person without %s has %s with probability %f'(g.disease_name,g.symptom,g.p_s_f)
+    sentence = 'A person without %s has %s with probability %f'(g.disease_name,g.symptom,g.p_s_nf)
     return sentence
 
+def question(g):
+    sentence = 'What is the probability that a person selected uniformly at random has has %s, given that he or she has %s?'%(g.disease_name,g.symtom)
+    return sentence
 #
 
 def get_disease_names():
     diseases = ['CSphobia','Biophobia','Mathphobia'] #TODO: re-think
     #
-    
-    return
+    rand_i = random.randint(a=0,b=len(diseases)) # Return a random integer N such that a <= N <= b.
+    disease_name = diseases[rand_i]
+    return disease_name
 
+def get_p_f():
+    return random.uniform(a=0, b=1) # Return a random floating point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
+
+def get_p_s_f():
+    return random.uniform(a=0, b=1) # Return a random floating point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
+
+def get_p_s_nf():
+    return random.uniform(a=0, b=1) # Return a random floating point number N such that a <= N <= b for a <= b and b <= N <= a for b < a.
+
+def solution_creater(g):
+    p_f = g.p_f
+    p_s_f = g.p_s_f
+    p_s_nf = g.p_s_nf
+    p_nf = 1 - p_f
+    #
+    p_s = p_s_f*p_f + p_s_nf*p_nf
+    # bayes theorem
+
+    return
 ##
 
 def probability_example_problem():
     first_seq_gen = [intro_sent]
     permutation_gen = [prob_disease_sent,prob_shaky_given_disease_sent,prob_shaky_given_no_disease_sent]
+    last_seq_gen = [question]
     #
-    key_words_values = {'disease_name':}
+    key_words_values = {'disease_name':get_disease_names, 'p_f':get_p_f, 'p_s_nf':get_p_s_nf}
+    #
+    first_seq_gen = SeqGen([intro_sent])
+    permutation_gen = PerGen([prob_disease_sent,prob_shaky_given_disease_sent,prob_shaky_given_no_disease_sent])
+    last_seq_gen = SeqGen([question])
+    generators = [first_seq_gen,permutation_gen,last_seq_gen]
+    problem = Problem(generators)
+    #
+    problem.
     return
 
 if __name__ = '__main__':
