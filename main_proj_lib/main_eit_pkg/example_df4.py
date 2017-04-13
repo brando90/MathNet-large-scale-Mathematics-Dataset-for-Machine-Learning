029de5567095fd19c39b75986ee583c8442bbdd8
Brando Miranda <brando90@mit.edu>
2017-04-13 19:10:29 -0400

    decorator examples and dataflow version 4

---
+++ main_proj_lib/main_eit_pkg/example_df4.py
@@ -0,0 +1,25 @@
+from dataflow import dataflow, Variable
+
+@dataflow
+def f(x, y):
+    return x**2 % y
+
+@dataflow
+def g(a, b, c):
+    return a * b + c
+
+@dataflow
+def h(i, j):
+    return i / j
+# h = dataflow(h)
+
+z = Variable()
+seed = Variable()
+
+# setting up the execution / dataflow
+graph = f(g(z, 2, h(4, z)), seed)
+# no addition or multiplication has happened yet
+
+# executing the dataflow
+print(graph.execute({z: 1., seed: 5}))
+print(graph.execute({z: 3., seed: 13}))
