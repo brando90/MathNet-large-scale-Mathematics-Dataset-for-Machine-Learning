dc4b36b3ffdcaf17c8a7b81dfd462a5869256db2
Brando Miranda <brando90@mit.edu>
2017-04-14 20:37:45 -0400

    unknwon commit

--- main_proj_lib/main_eit_pkg/question_answer.py
+++ main_proj_lib/main_eit_pkg/question_answer.py
@@ -9,9 +9,14 @@ def make_qa_pair(question,answer,assignments={},seed=None):
     '''
     Given a question, answer (and optional assignments) written with the funcflow
     framework, returns a tuple with two strings with the question and answer.
+
+    If node seed is used, it doesn't explicitly set a seed. If you want a seed
+    from the os do:
+    One suggestion for seeding a random seed: seed = int.from_bytes(os.urandom(4), sys.byteorder)
     '''
     #np.random.seed(arg.rand_x)
     #tf.set_random_seed( arg.rand_x )
+    # seed = int.from_bytes(os.urandom(4), sys.byteorder)
     if seed != None:
         random.seed(seed)
     assignments = { key: [random.sample(value,1)[0]] for key,value in assignments.items() }
