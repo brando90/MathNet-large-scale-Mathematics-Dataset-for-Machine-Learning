a9f029c31affd81055edc5c3d9c32f910eb29ecb
Brando Miranda <brando90@mit.edu>
2017-04-18 12:49:36 -0400

    exmaples for yasmin

--- lstm_example/reader.py
+++ lstm_example/reader.py
@@ -1,23 +1,3 @@
-
-
-
-
-# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
-
 """Utilities for parsing PTB text files."""
 from __future__ import absolute_import
 from __future__ import division
@@ -25,19 +5,29 @@ from __future__ import print_function
 
 import collections
 import os
+import pdb
 
 import tensorflow as tf
 
 
+
 def _read_words(filename):
+  '''
+  returns an array of strings with each word:
+
+  E.g: ['solve', 'x', ',', 'Eq(a,', 'b)', ',', 'Eq(x,', '2*b)', ',', 'Eq(a,', '19)', ',', 'can', 'you', 'do', 'it?']
+  '''
+  # TODO: doesn't seem that its processing the equations correctly
   with tf.gfile.GFile(filename, "r") as f:
     return f.read().decode("utf-8").replace("\n", "<eos>").split()
 
 
 def _build_vocab(filename):
-  data = _read_words(filename)
+  data = _read_words(filename) # array of words: ['solve', 'x', ',', 'Eq(a,', 'b)', ',', 'Eq(x,', '2*b)', ',', 'Eq(a,', '19)', ',', 'can', 'you', 'do', 'it?']
+  #pdb.set_trace()
 
   counter = collections.Counter(data)
+  pdb.set_trace()
   count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
 
   words, _ = list(zip(*count_pairs))
