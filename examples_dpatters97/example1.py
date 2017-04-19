00be8f79e9b6ef263879c72397ffd9686a24dcf2
Ubuntu <damarcus@mit.edu>
2017-04-19 18:17:38 -0400

    Made first example

--- examples_dpatters97/example1.py
+++ examples_dpatters97/example1.py
@@ -1,1 +1,22 @@
-#Damarcus first exmaple
+from sympy import *
+import random
+
+from qaflow import *
+from qaflow.question_answer import *
+
+def ex1():
+	x, a, b = symbols('x a b')
+	assignments = {}
+	assignments[x] = [a, b, x]
+
+	expr = x**2
+	question = seqg('Find derivative of ', expr, 'with respect to ', x)
+	@func_flow
+	def solve_deriv(ex, resp):
+		return diff(ex, resp)
+	ans = choiceg(solve_deriv(expr, x))
+	q,a = make_qa_pair(question, ans, assignments)
+	print('question {0}\nanswer{1}'.format(q, a))
+
+if __name__ == '__main__':
+	ex1()
