1674f149e2af67da6f32760aa745207f650be383
Brando Miranda <brando90@mit.edu>
2017-05-02 00:15:41 -0400

    change variables name assignment to replacement for clarity

--- examples_brando90/simple_examply.py
+++ examples_brando90/simple_examply.py
@@ -16,16 +16,16 @@ def example2():
     e,f,g,h = symbols('e f g h')
     ## solve x,  a = b,  x = 2*b, a = 8, can you do it?
     question = seqg(seqg('solve ',x,', '), perg( seqg(Eq(a,b),', ') , seqg(Eq(x,2*b),', ') , seqg(Eq(a,8),', ') ), 'can you do it?')
-    ## pssible alternative assignments for symbols
-    assignments = {}
-    assignments[a] = [a,e,f,g,h]
-    assignments[x] = [x,X]
-    assignments[8] = [random.randint(0,200) for i in range(100)]
+    ## pssible alternative replacements for symbols
+    replacements = {}
+    replacements[a] = [a,e,f,g,h]
+    replacements[x] = [x,X]
+    replacements[8] = [random.randint(0,200) for i in range(100)]
     ## generator for a choice of answer
     ans1 = seqg(x,' = ',2,'*',8)
     ans2 = seqg(x,'=',2,'*',8)
     answer = choiceg( ans1,ans2 )
-    q,a = make_qa_pair(question,answer,assignments,seed=4)
+    q,a = make_qa_pair(question,answer,replacements,seed=4)
     print('question: %s \nanswer: %s'%(q,a))
 
 
@@ -34,11 +34,11 @@ def example3():
     e,f,g,h = symbols('e f g h')
     ## solve x,  a = b,  x = 2*b, a = 8, can you do it?
     question = seqg(seqg('solve ',x,', '), perg( seqg(Eq(a,b),', ') , seqg(Eq(x,2*b),', ') , seqg(Eq(a,8),', ') ), 'can you do it?')
-    ## pssible alternative assignments for symbols
-    assignments = {}
-    assignments[a] = [a,e,f,g,h]
-    assignments[x] = [x,X]
-    assignments[8] = [random.randint(0,200) for i in range(100)]
+    ## pssible alternative replacements for symbols
+    replacements = {}
+    replacements[a] = [a,e,f,g,h]
+    replacements[x] = [x,X]
+    replacements[8] = [random.randint(0,200) for i in range(100)]
     ## possible answer(s)
     ans1 = seqg(x,' = ',2,'*',8)
     ans2 = seqg(x,'=',2,'*',8)
@@ -49,9 +49,9 @@ def example3():
     ans4 = seqg(x, ' = ', seqg( my_mult(2,8) ) )
     ## generator for a choice of answer
     answer = choiceg( ans1,ans2,ans3,ans4 )
-    q,a = make_qa_pair(question,answer,assignments,seed=5)
+    q,a = make_qa_pair(question,answer,replacements,seed=5)
     #print('question: %s \nanswer: %s'%(q,a))
-    return question,answer,assignments
+    return question,answer,replacements
 
 def example4():
     '''
@@ -66,9 +66,9 @@ def example4():
     make_and_check_dir(path=location)
     # get question, answer parameters
     nb_data_points = 500
-    question,answer,assignments = example3()
+    question,answer,replacements = example3()
     # make data set
-    make_qa_to_data_set(question,answer,assignments,nb_data_points, location,question_name,answer_name)
+    make_qa_to_data_set(question,answer,replacements,nb_data_points, location,question_name,answer_name)
 
 
 if __name__ == '__main__':
