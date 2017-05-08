1797a31d7366d63e64008e5a9606a711887bee36
Yasmin <Siahpoosh@dhcp-18-189-25-202.dyn.mit.edu>
2017-05-08 18:10:46 -0400

    added coin flip probability example

--- examples_ysiahpoosh/find_slope.py
+++ examples_ysiahpoosh/find_slope.py
@@ -26,6 +26,35 @@ def example1():
 	q,a = make_qa_pair(question, answer, assignments, seed=3)
 	print('question: %s \nanswer: %s'%(q,a))
 
+
+def example2():
+	#What is the slope of the line through two points 
+	question = seqg('What is the slope of the line through the following points: ', perg(seqg('(', 1, ',', 2, ')'),seqg('(', 3, ',', 4, ')') ), '?')
+
+	#possible assignments for numbers
+	assignments = {}
+	for i in range(1,5):
+		assignments[i] = [random.randint(-10,10) for j in range(100)]
+
+	@func_flow
+	def find_slope(a,b,c,d):
+		if c-a == 0:
+			return "undefined"
+		return (d-b)/(c-a)
+
+	@func_flow
+	def slope_as_frac(a,b,c,d):
+		if c-a == 0:
+			return "undefined"
+		return Rational(d-b, c-a)
+
+	ans1 = find_slope(1,2,3,4)
+	ans2 = slope_as_frac(1,2,3,4)
+	answer = choiceg(ans1, ans2)
+	q,a = make_qa_pair(question, answer, assignments, seed=3)
+	print('question: %s \nanswer: %s'%(q,a))
+
 if __name__=='__main__':
-	example1()
+	#example1()
+	example2()
 
