223183b1ff7c476fe2ce0d5c79fca0fe23f6eecb
ivanjut333 <ivan.cal333@gmail.com>
2017-05-09 18:09:50 -0400

    added cubic term

--- examples_ivanjut/example_integrate.py
+++ examples_ivanjut/example_integrate.py
@@ -31,5 +31,19 @@ def example2():
     q,a = make_qa_pair(question,answer,replacements,seed=1)
     print('question: %s \nanswer: %s'%(q,a))
 
+
+def example3():
+    ## integrate the following expression x**2 + x + 1 with respect to x.
+    ## integrate perm(x**2 + x + 1) with respect to x.
+    x,y,a,b, X = symbols('x y a b X')
+    e,f,g,h = symbols('e f g h')
+    expression = perg( x**3, '+', x**2, '+', x,'+', 1)
+    question = seqg('integrate the following expression ', expression, )
+    replacements = {}
+    replacements[x] = [x,y,a,b,X, e,f,g,h]
+    answer = choiceg( integrate(x**3+x**2 + x + 1) )
+    q,a = make_qa_pair(question,answer,replacements,seed=1)
+    print('question: %s \nanswer: %s'%(q,a))
+
 if __name__ == '__main__':
-    example2()
+    example3()
