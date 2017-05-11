13c1c4e70e296f98d5dac51ed6f6be73dd36c36c
ivanjut333 <ivan.cal333@gmail.com>
2017-05-10 20:34:59 -0400

    updated integrate

--- examples_ivanjut/example_integrate.py
+++ examples_ivanjut/example_integrate.py
@@ -4,9 +4,12 @@ import random
 from qaflow import *
 from qaflow.question_answer import *
 
+
 def example1():
-    ## integrate the following expression x**2 + x + 1 with respect to x.
-    ## integrate x**2 + x + 1 with respect to x.
+
+    # integrate the following expression x**2 + x + 1 with respect to x.
+    # integrate x**2 + x + 1 with respect to x.
+
     x,y,a,b, X = symbols('x y a b X')
     e,f,g,h = symbols('e f g h')
     expression = x**2 + x + 1
@@ -18,12 +21,15 @@ def example1():
     q,a = make_qa_pair(question,answer,replacements,seed=1)
     print('question: %s \nanswer: %s'%(q,a))
 
+
 def example2():
-    ## integrate the following expression x**2 + x + 1 with respect to x.
-    ## integrate perm(x**2 + x + 1) with respect to x.
-    x,y,a,b, X = symbols('x y a b X')
-    e,f,g,h = symbols('e f g h')
-    expression = perg( x**2, '+', x,'+', 1)
+
+    # integrate the following expression x**2 + x + 1 with respect to x.
+    # integrate perm(x**2 + x + 1) with respect to x.
+
+    x, y, a, b, X = symbols('x y a b X')
+    e, f, g, h = symbols('e f g h')
+    expression = perg(x**2, '+', x, '+', 1)
     question = seqg('integrate the following expression ', expression, )
     replacements = {}
     replacements[x] = [x,y,a,b,X, e,f,g,h]
@@ -33,11 +39,13 @@ def example2():
 
 
 def example3():
-    ## integrate the following expression x**2 + x + 1 with respect to x.
-    ## integrate perm(x**2 + x + 1) with respect to x.
+
+    # integrate the following expression x**3 + x**2 + x + 1 with respect to x.
+    # integrate perm(x**3 + x**2 + x + 1) with respect to x.
+
     x,y,a,b, X = symbols('x y a b X')
     e,f,g,h = symbols('e f g h')
-    expression = perg( x**3, '+', x**2, '+', x,'+', 1)
+    expression = perg(x**3, '+', x**2, '+', x, '+', 1)
     question = seqg('integrate the following expression ', expression, )
     replacements = {}
     replacements[x] = [x,y,a,b,X, e,f,g,h]
@@ -45,5 +53,8 @@ def example3():
     q,a = make_qa_pair(question,answer,replacements,seed=1)
     print('question: %s \nanswer: %s'%(q,a))
 
+
 if __name__ == '__main__':
+    # example1()
+    # example2()
     example3()
