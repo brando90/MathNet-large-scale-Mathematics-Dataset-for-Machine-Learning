f55ed6f36a6f59cc374b8a7f20acc0ef17047e7e
Brando Miranda <brando90@mit.edu>
2017-05-16 16:24:10 -0400

    added a permutable part to the physics question

--- examples_brookssj/velocity_and_acceleration.py
+++ examples_brookssj/velocity_and_acceleration.py
@@ -14,7 +14,11 @@ from qaflow.question_answer import *
 def example1():
     t,v,a=symbols('t v a')
     ##find the distance traveled in t seconds, initial velocity is v m/s, acceleration is a m/s^2
-    question = seqg('find the distance traveled in ', t , 'seconds: ','initial velocity is ', v, 'm/s, ','acceleration is ', a, 'm/s^2')
+    #question = seqg('find the distance traveled in ', t , 'seconds: ','initial velocity is ', v, 'm/s, ','acceleration is ', a, 'm/s^2')
+    vel_part = seqg('initial velocity is ', v)
+    acc_part = seqg('acceleration is ', a, 'm/s^2')
+    permutable_part = perg(vel_part, acc_part)
+    question = seqg('find the distance traveled in ', t , 'seconds if ', permutable_part )
     assignments = {}
     sym_list=[t, v, a]
     for i in sym_list:
@@ -25,7 +29,8 @@ def example1():
         return v*t+.5*a*t**2
 
     answer = find_dist(t, v, a)
-    q,a = make_qa_pair(question,answer,assignments, seed=2)
+    #q,a = make_qa_pair(question,answer,assignments, seed=2)
+    q,a = make_qa_pair(question,answer,assignments)
     print('question: %s \nanswer: %s'%(q,a))
 
 if __name__ == '__main__':
