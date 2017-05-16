cd45b0be80f19eacc66869639fc3e4ff547e57fb
Brando Miranda <brando90@mit.edu>
2017-05-16 16:52:46 -0400

    now with some repalcements

--- examples_brookssj/integrate_log_2_x.py
+++ examples_brookssj/integrate_log_2_x.py
@@ -12,14 +12,17 @@ from qaflow import *
 from qaflow.question_answer import *
 
 def example1():
-    x = symbols('x')
+    x,a,b,c,d,e,f,g= symbols('x a b c d e f g')
     expression_q = log(x)**2
     # Can you integrate this:  log(x)**2 expression?
     question = seqg('Can you integrate this: ' , expression_q,' ?')
 
+    assignments = {}
+    assignments[x] = [x,a,b,c,d,e,f,g]
+
     expression_ans = Eq(Integral(log(x)**2, x), Integral(log(x)**2, x).doit() )
     answer = seqg(  expression_ans ) # x⋅log (x) - 2⋅x⋅log(x) + 2⋅x
-    q,a = make_qa_pair(question,answer)
+    q,a = make_qa_pair(question,answer,assignments)
     print('question: %s \nanswer: %s'%(q,a))
 
 if __name__ == '__main__':
