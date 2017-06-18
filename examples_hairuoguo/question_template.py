c3570cea47842b1d1b74f65c0c2191f64c996745
hairuo <hairuoguo@gmail.com>
2017-06-18 02:06:59 -0700

    Added comments, changed name and sympy_vars fields in QA to list representations from set, wrote second example, updated question template.

--- examples_hairuoguo/question_template.py
+++ examples_hairuoguo/question_template.py
@@ -6,28 +6,28 @@ class TemplateMC(QAFormat):
         QAFormat.__init__(variable_seed, correct_seed)
          
     def generate_q(self, seed):
-    '''formatting for question'''
+    '''created formatted string for question'''
         self.question.set_expression(question_expression)
         question_expression = self.question.create_expression(seed)
     '''Implement this method below'''
         except NotImplementedError
     
     def generate_a(self, seed):
-    '''formatting for answer'''
+    '''create formatted string for answer'''
         self.answer.set_expression(answer_expression)
         answer_expression = self.answer.create_expression(seed)
     '''Implement this method below'''
         except NotImplementedError
     
     def question_expression(self, seed):
-    '''generate the expression for the question'''
+    '''generate the mathematical expressions for the question'''
         np.random.seed(seed)
     '''Implement this method below'''
         #TODO: maybe something that would make it so that you only had to declare these variables once across the question and answer?
         except NotImplementedError
 
     def answer_expression(self, seed):
-    '''generate the expression for the answer'''
+    '''generate the mathematical expressions for the answer'''
         np.random.seed(seed)
     '''Implement this method below'''
         except NotImplementedError
