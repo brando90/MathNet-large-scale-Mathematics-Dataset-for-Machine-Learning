98b58e078c167e185ee4391ed227bcf111b0cdde
hairuo <hairuoguo@gmail.com>
2017-06-19 17:35:38 -0700

    Changed seeding format to include init() and init_consistent() corresponding to issue #26.

--- examples_hairuoguo/prototype_question.py
+++ examples_hairuoguo/prototype_question.py
@@ -4,15 +4,15 @@ from class_prototype import *
 
 class SimpleQuestionExample1(QAFormat):
     
-    def __init__(self, variable_seed, correct_seed):
-        QAFormat.__init__(self, variable_seed, correct_seed)
+    def __init__(self):
+        QAFormat.__init__(self)
          
     def generate_q(self, seed):
         '''output formatted string for question'''
         self.question.set_expression(self.question_expression)
         '''Implement this method below'''
         #anything involve permutations would probably go here
-        question = self.question.create_expression(seed)
+        question = self.question.create_expression(*self.const_vars, *self.vars)
         return "Solve: %s, %s, find %s" % question
         
     
@@ -20,77 +20,45 @@ class SimpleQuestionExample1(QAFormat):
         '''output formatted string for answer'''
         self.answer.set_expression(self.answer_expression)
         '''Implement this method below'''
-        answer = self.answer.create_expression(seed)
+        answer = self.answer.create_expression(*self.const_vars, *self.vars)
         return str(answer)
- 
-    def question_expression(self, seed):
-        '''generate the mathematical expressions for the question'''
-        np.random.seed(seed)
-        '''Implement this method below'''
-        var1 = self.question.get_symbol()
-        var2 = self.question.get_symbol()
-        var3 = self.question.get_symbol()
 
-        expr1 = sympy.Eq(var1, var2)
-        expr2 = sympy.Eq(var2, var3)
+    def create_const_variables(self):
+        var1 = self.get_symbol()
+        var2 = self.get_symbol()
+        var3 = self.get_symbol()
         
-        return expr1, expr2, var1
+        return var1, var2, var3
 
-    def answer_expression(self, seed):
-        '''generate the mathematical expressions for the answer'''
-        np.random.seed(seed)
-        '''Implement this method below'''
-        var1 = self.answer.get_symbol()
-        var2 = self.answer.get_symbol()
-        var3 = self.answer.get_symbol()
-
-        expr = sympy.Eq(var1, var3)
-
-        return expr
-
-class SimpleQuestionExample2(SimpleQuestionExample1):
-    #We can make this a subclass of the first example because they share the same question formatting
-     
-    def __init__(self, variable_seed, correct_seed):
-        QAFormat.__init__(self, variable_seed, correct_seed)
-
-    
-    def question_expression(self, seed):
-        '''generate the mathematical expressions for the question'''
-        np.random.seed(seed)
-        '''Implement this method below'''
-        var1 = self.question.get_symbol()
-        var2 = self.question.get_symbol()
-        var3 = self.question.get_symbol()
+    def create_variables(self):
         
         const1 = np.random.randint(1, 10)
         const2 = np.random.randint(1, 10)
+        
+        return const1, const2
 
+    def question_expression(self, var1, var2, var3, const1, const2):
+        '''generate the mathematical expressions for the question'''
+        '''Implement this method below'''
         expr1 = sympy.Eq(var1, const1*var2)
         expr2 = sympy.Eq(var2, var3/const2)
 
         return expr1, expr2, var1
 
-    def answer_expression(self, seed):
+    def answer_expression(self, var1, var2, var3, const1, const2):
         '''generate the mathematical expressions for the answer'''
-        np.random.seed(seed)
         '''Implement this method below'''
-        var1 = self.answer.get_symbol()
-        var2 = self.answer.get_symbol()
-        var3 = self.answer.get_symbol()
-
-        const1 = np.random.randint(1, 10)
-        const2 = np.random.randint(1, 10)
 
         expr = var3*const1/const2
 
         return expr
-        
+ 
 
-class SimpleQuestionExample3(QAFormat):
 
-    def __init__(self, variable_seed, correct_seed):
-        QAFormat.__init__(self, variable_seed, correct_seed)
+class SimpleQuestionExample2(QAFormat):
+
+    def __init__(self):
+        QAFormat.__init__(self)
         
     def generate_q(self, seed):
         '''formatting for question'''
@@ -121,14 +89,11 @@ class SimpleQuestionExample3(QAFormat):
 
  
 if __name__ == "__main__":
-    example1_instance = SimpleQuestionExample1(1, 1)
-    example2_instance = SimpleQuestionExample2(1, 1)
-    print(example1_instance.generate_mc_q(0))
-    print(example1_instance.generate_mc_q(0))
-    print(example2_instance.generate_mc_q(1))
-    print(example2_instance.generate_mc_q(1))
-    print(example1_instance.generate_mc_a(4, 0))
-    print(example1_instance.generate_mc_a(4, 0))
-    print(example2_instance.generate_mc_a(4, 1))
-    print(example2_instance.generate_mc_a(3, 1))
+    example1 = SimpleQuestionExample1()
+    example1.set_correct_seed(1)
+    example1.init_consistent(1)
+    print(example1.generate_mc_q(0))
+    print(example1.generate_mc_q(0))
+    print(example1.generate_mc_a(4, 0))
+    print(example1.generate_mc_a(4, 0))
  
