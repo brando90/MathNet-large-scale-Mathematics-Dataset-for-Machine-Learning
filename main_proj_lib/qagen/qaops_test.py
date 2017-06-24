cf13475c43bdddcc47d05d9b8acb73586b26f795
Brando Miranda <brando90@mit.edu>
2017-06-24 18:10:12 -0400

    small changes

--- main_proj_lib/qagen/qaops_test.py
+++ main_proj_lib/qagen/qaops_test.py
@@ -1,1 +1,13 @@
+from sympy import *
 import unittest
+
+import utils
+
+class Test_QAOps(unittest.TestCase):
+
+    def test_(self):
+        # TODO
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
