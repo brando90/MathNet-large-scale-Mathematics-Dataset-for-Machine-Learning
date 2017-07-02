bf3da902604a748ccb4b90f16083e08d22c443c7
brookssj <sjbrooks@mit.edu>
2017-07-01 21:37:01 -0400

    Old to new format

---
+++ math_taxonomy/physics/classical_mechanics/velocity_and_acceleration.py
@@ -0,0 +1,209 @@
+from sympy import *
+import random
+import numpy as np
+
+from qagen import qagen
+from qagen import utils
+#from qagen import unit_test_for_user as user_test
+
+# TODO: You can also put your quesiton example here
+
+class QA_constraint(QAGen):
+
+    def __init__(self):
+        '''
+        Initializer for your QA question.
+        '''
+        super().__init__()
+        self.author = 'Skylar Brooks' #TODO your full name
+        self.description = 'What is the distance Hannah traveles in t = 10 seconds if her initial velocity is v=3 m/s and her acceleration is a=2 m/s^2?' #TODO example string of your question
+        # keywords about the question that could help to make this more searchable in the future
+        self.keywords = ['physics', 'word problem'] #TODO keywords to search type of question
+        self.use_latex = True
+
+    def seed_all(self,seed):
+        '''
+        Write the seeding functions of the libraries that you are using.
+        Its important to seed all the libraries you are using because the
+        framework will assume it can seed stuff for you. It needs this for
+        the library to work.
+        '''
+        random.seed(seed)
+        np.random.seed(seed)
+        fake.random.seed(seed)
+        # TODO write more seeding libraries that you are using
+
+    def init_consistent_qa_variables(self):
+        """
+        Defines and returns all the variables that need to be consistent
+        between a question and an answer. Usually only names and variable/symbol
+        names.
+
+        Example: when generating MC questions the non consistent variables will
+        be used to generate other options. However, the names, symbols, etc
+        should remain consistent otherwise some answers will be obviously fake.
+
+        Note: debug flag can be used to deterministically output a QA that has
+        simple numbers to check the correctness of your QA.
+        """
+        if self.debug:
+            Hannah = 'Hannah'
+            t, v, a = symbols ('t v a')
+        else:
+            Hannah = self.get_names(1)
+            t, v, a = self.get_symbols(3)
+        return Hannah,t,v,a
+
+    def init_qa_variables(self):
+        '''
+        Defines and returns all the variables that can vary between a
+        question and an answer. Good examples are numerical values that might
+        make the answers not obviously wrong.
+
+        Example: when generating MC questions the non consistent variables will
+        be used to generate other options. However, the names, symbols, etc
+        should remain consistent otherwise some answers will be obviously fake.
+        Numerical values that have been fully evaluated are a good example of
+        how multiple choice answers can be generated.
+
+        Note: debug flag can be used to deterministically output a QA that has
+        simple numbers to check the correctness of your QA.
+        '''
+        if self.debug:
+            t_val = 10
+            v_val = 3
+            a_val = 2
+        else:
+            t_val = np.random.randint(100)
+            v_val = np.random.randint(10)
+            a_val = np.random.randint(10)
+        return t_val,v_val,a_val
+
+    def Q(s, t_val,v_val,a_val, Hannah,t,v,a): #TODO change the signature of the function according to your question
+        '''
+        Small question description.
+
+        Important Note: first variables are the not consistent variables followed
+        by the consistent ones. See sample QA example if you need too.
+        '''
+        #define some short cuts
+        seqg, perg, choiceg = s.seqg, s.perg, s.choiceg
+        s.use_latex = True
+        # TODO
+        #q_format1
+        #q_format2
+        #...
+        # choices, try providing a few
+        # these van include variations that are hard to encode with permg or variable substitution
+        # example, NL variations or equaiton variations
+        question1 = seqg('What is the distance ', Hannah, ' traveles in ', Eq(t, t_val), ' seconds if her initial velocity is ', Eq(v, v_val), ' m/s and her acceleration is ', Eq(a, a_val), 'm/s^2?')
+        q = choiceg(question1)
+        return q
+
+    def A(s, t_val,v_val,a_val, Hannah,t,v,a): #TODO change the signature of the function according to your answer
+        '''
+        Small answer description.
+
+        Important Note: first variables are the not consistent variables followed
+        by the consistent ones. See sample QA example if you need too.
+        '''
+        #define some short cuts
+        seqg, perg, choiceg = s.seqg, s.perg, s.choiceg
+        # TODO
+        #ans_sympy
+        #ans_numerical
+        #ans_vnl_vsympy1
+        #ans_vnl_vsympy2
+        # choices, try providing a few
+        # these van include variations that are hard to encode with permg or variable substitution
+        # example, NL variations or equaiton variations
+        ans1= seqg(Hannah + ' travels ' + v_val*t_val+.5*a_val*t_val**2 + ' m in ' + Eq(t, t_val) + 'seconds') 
+        ans2= seqg('In ', Eq(t, t_val), 'seconds, ', Hannah, ' travels ', v_val*t_val+.5*a_val*t_val**2,' m.')        
+        a = choiceg(ans1, ans2)
+        return a
+
+    ##
+
+    def get_qa(self,seed):
+        '''
+        Example of how Q,A are formed in general.
+        '''
+        # set seed
+        self.seed_all(seed)
+        # get variables for qa and register them for the current q,a
+        variables, variables_consistent = self._create_all_variables()
+        # get concrete qa strings
+        q_str = self.Q(*variables,*variables_consistent)
+        a_str = self.A(*variables,*variables_consistent)
+        return q_str, a_str
+
+## Some helper functions to check the formats are coming out correctly
+
+##
+
+def check_single_question_debug(qagenerator):
+    '''
+    Checks by printing a single quesiton on debug mode
+    '''
+    qagenerator.debug = True
+    q,a = qagenerator.get_qa(seed=1)
+    print('qagenerator.debug = ', qagenerator.debug)
+    print('q: ', q)
+    print('a: ', a)
+
+def check_single_question(qagenerator):
+    '''
+    Checks by printing a single quesiton on debug mode
+    '''
+    q,a = qagenerator.get_qa(seed=random.randint(0,1000))
+    print('qagenerator.debug = ', qagenerator.debug)
+    print('q: ', q)
+    print('a: ', a)
+
+def check_mc(qagenerator):
+    '''
+    Checks by printing the MC(Multiple Choice) option
+    '''
+    nb_answers_choices = 10
+    for seed in range(3):
+        #seed = random.randint(0,100)
+        q_str, ans_list = qagenerator.generate_single_qa_MC(nb_answers_choices=nb_answers_choices,seed=seed)
+        print('\n-------seed-------: ',seed)
+        print('q_str:\n',q_str)
+        print('-answers:')
+        print("\n".join(ans_list))
+
+def check_many_to_many(qagenerator):
+    for seed in range(3):
+        q,a = qagenerator.generate_many_to_many(nb_questions=4,nb_answers=3,seed=seed)
+        print('-questions:')
+        print("\n".join(q))
+        print('-answers:')
+        print("\n".join(a))
+
+def check_many_to_one_consis(qagenerator):
+    for seed in range(3):
+        print()
+        q,a = qagenerator.generate_many_to_one(nb_questions=5,seed=seed)
+        print("\n".join(q))
+        print('a: ', a)
+        #print("\n".join(a))
+
+def check_many_to_one_consistent_format(qagenerator):
+    nb_qa_pairs,nb_questions = 10,3
+    qa_pair_list = qagenerator.generate_many_to_one_consistent_format(nb_qa_pairs,nb_questions)
+    for q_list,a_consistent_format in qa_pair_list:
+        print()
+        print("\n".join(q_list))
+        print('a: ', a_consistent_format)
+
+if __name__ == '__main__':
+    qagenerator = QA_constraint()
+    check_single_question(qagenerator)
+    ## uncomment the following to check formats:
+    #check_mc(qagenerator)
+    #check_many_to_one(qagenerator)
+    #check_one_to_many(qagenerator)
+    #check_many_to_one_consistent_format(qagenerator)
+    ## run unit test given by framework
+    #user_test.run_unit_test_for_user(QA_constraint)
